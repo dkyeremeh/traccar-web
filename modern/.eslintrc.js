064e4c117b9ac1eded69d53049d99ea7eab95c59
Desmond Kyeremeh <elDekyfin@gmail.com>
2021-07-09 22:14:26 +0000

    Updated eslint config

---
+++ modern/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'arrow-parens': 0,
+    'comma-dangle': ['warn', 'always-multiline'],
+    'no-restricted-syntax': 0,
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, minProperties: 4, consistent: true },
+      ObjectPattern: { multiline: true, minProperties: 4, consistent: true },
+      ImportDeclaration: { multiline: true, minProperties: 8, consistent: true },
+      ExportDeclaration: { multiline: true, minProperties: 4, consistent: true },
+    }],
+    'operator-linebreak': 'off',
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+  },
+};
