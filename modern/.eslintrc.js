5245959db89509bbfbad71ee2e8ac6e895727bca
Desmond Kyeremeh <elDekyfin@gmail.com>
2021-07-09 23:01:29 +0000

    Updated eslint

--- modern/.eslintrc.js
+++ modern/.eslintrc.js
@@ -18,7 +18,10 @@ module.exports = {
   rules: {
     'arrow-parens': 0,
     'comma-dangle': ['warn', 'always-multiline'],
+    'func-names': 0,
+    'no-param-reassign': ['warn', { props: false }],
     'no-restricted-syntax': 0,
+    'no-return-assign': ['warn', 'except-parens'],
     'object-curly-newline': ['error', {
       ObjectExpression: { multiline: true, minProperties: 4, consistent: true },
       ObjectPattern: { multiline: true, minProperties: 4, consistent: true },
